b15bde7476e33e2b0e1a77ec05a5b53f1c9bedc9
Sandra Malinova <90518039+MalininiGrande@users.noreply.github.com>
2022-03-16 21:16:47 +0100

    odevzdaniUkoluPoOprave

--- vyvolavac.js
+++ vyvolavac.js
@@ -28,69 +28,37 @@ let jmena = [
 ];
 
 let tazenaJmena = [];
-<<<<<<< HEAD
-=======
 let points = document.querySelector(".points");
-let score = 1;
+let score = 0;
 
->>>>>>> develop-2022
 
 function tahnoutJmeno() {
     if (jmena.length === 0) {
         return;
     }
-<<<<<<< HEAD
-=======
    
->>>>>>> develop-2022
 
     // Generujeme náhodný index
     let vyherniIndex = Math.floor(Math.random() * jmena.length);
 
-<<<<<<< HEAD
-    let vyherniIndex = Math.floor(Math.random() * jmena.length);
-
     // Získáme výherní jméno na patřičném indexu
     let vyherniJmeno = jmena[vyherniIndex];
 
-    let vyherniJmeno = jmena[vyherniIndex];
-
-=======
-    // Získáme výherní jméno na patřičném indexu
-    let vyherniJmeno = jmena[vyherniIndex];
-
->>>>>>> develop-2022
     // Vyřadíme vylosované jméno z osudí
     jmena.splice(vyherniIndex, 1);
 
     // Výherní jméno si uložíme do pole k ostatním výherním
-<<<<<<< HEAD
-    tazenaJmena.push(vyherniJmeno);
-<<<<<<< HEAD:main.js
+
+    // tazenaJmena.push(vyherniJmeno);
+
     let vyherce = document.querySelector("#vyherka");
     vyherce.textContent = vyherniJmeno;
 
     let seznam = document.querySelector("#seznam");
     seznam.textContent = tazenaJmena;
     
-=======
-
-    let vyherce = document.querySelector('#vyherka');
-    vyherce.innerHTML = vyherniJmeno;
-
-    let seznamJmen = document.querySelector('#seznam');
-    seznamJmen.innerHTML = tazenaJmena;
->>>>>>> 0522cdcef4205de17cde2d422ab3e34312778d00:vyvolavac.js
-}
-=======
     tazenaJmena.unshift(vyherniJmeno);
 
-
-    let vyherce = document.querySelector('#vyherka');
-    vyherce.textContent = vyherniJmeno;
-
-    let seznam = document.querySelector('#seznam');
-    seznam.textContent = tazenaJmena;
 }
 
 function skore() {
@@ -100,4 +68,4 @@ function skore() {
 function restart() {
     document.location.reload();
   }
->>>>>>> develop-2022
+
